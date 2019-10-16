8911e36ef88f6115382ee584e4a14ba02ddc2724
Adam Bialek <adamjanbialek@gmail.com>
2019-10-16 12:26:19 +0200

    dodane testy

--- src/test/java/LiczbaPierwszaTest.java
+++ src/test/java/LiczbaPierwszaTest.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
 
-public class LiczbaPierwszaTest {
-}
+    import org.junit.Test;
+import org.junit.Assert;
+
+    public class LiczbaPierwszaTest {
+        @Test
+        public void shouldCheckIfLiczbaPierwsza(){
+
+            Assert.assertTrue(LiczbaPierwsza.czyLiczbaPierwsza(2));
+            Assert.assertTrue(LiczbaPierwsza.czyLiczbaPierwsza(1));
+        }
+    }
+
