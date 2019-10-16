c1c99dac17f909c585ceeea9f5e67884afc580ad
Adam Bialek <adamjanbialek@gmail.com>
2019-10-16 12:18:58 +0200

    funkcja czyLiczbaPierwsza

--- src/main/java/LiczbaPierwsza.java
+++ src/main/java/LiczbaPierwsza.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class LiczbaPierwsza {
+
+    public static Boolean czyLiczbaPierwsza(int a){
+        List<Integer> dzielniki=new ArrayList<Integer>();
+        boolean czyPierwsza = false;
+        for(int i=1; i<=a; i++) {
+            if(a%i==0){
+                dzielniki.add(i);
+            }
+        }
+
+        if(dzielniki.size()<3 && dzielniki.contains(1) && dzielniki.contains(a)){
+            czyPierwsza=true;
+        }
+
+        return czyPierwsza;
+    }
+
 }
