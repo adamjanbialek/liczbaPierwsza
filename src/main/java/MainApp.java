cdd7ff509e44064bdffa735d2a817efdbdeb0236
Adam Bialek <adamjanbialek@gmail.com>
2019-10-18 12:53:53 +0200

    changes

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class MainApp {
+
+    public static boolean podajLiczbeDoSprawdzenia() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbę dla której chcesz wyliczyć silnie.");
+        int a = scanner.nextInt();
+        int wyliczSilnia = Silnia.czySilnia(a);
+        System.out.println(wyliczSilnia);
+
+        boolean wynik=LiczbaPierwsza.czyLiczbaPierwsza(a);
+
+        return wynik;
+
+
+    }
+
+    public static void main(String[] args) {
+        podajLiczbeDoSprawdzenia();
+    }
+
+
 }
--- src/test/java/SilniaTest.java
+++ src/test/java/SilniaTest.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class SilniaTest {
+
+    @Test
+    public void shouldCalcFactorial() {
+        int expected=120;
+        Assert.assertEquals(expected,Silnia.czySilnia(5));
+    }
+
+    @Test
+    public void shouldCalcFactorialWhenZero() {
+        int expected=1;
+        Assert.assertEquals(expected,Silnia.czySilnia(0));
+    }
+
+    @Test
+    public void shouldCalcFactorialWhenNumberIsBelowZero(){
+        String expected = "Dla podanej liczby nie można wyliczyć silni";
+        Assert.assertEquals(expected,Silnia.czySilnia(-1));
+    }
+
+    @Test
+    public void shouldNaturalNumber(){
+        boolean expected=true;
+        Assert.assertEquals(expected,MainApp.podajLiczbeDoSprawdzenia());
+    }
 }
